255c1af5a7b29a4ca3ad3c5a8b9a3f4064d2c989
SteveHuy <130954328+SteveHuy@users.noreply.github.com>
2023-09-18 23:14:51 +1000

    Add files via upload

--- client/src/GlobalContext.js
+++ client/src/GlobalContext.js
@@ -6,10 +6,13 @@ export const GlobalContext = createContext();
 export const GlobalProvider = ({ children }) => {
 
   const [placementVariable, setPlacementVariable] = useState("");
+  const [plantData, setPlantData] = useState("");
+  const [recipeData, setRecipeData] = useState(null);
 
   return (
-    <GlobalContext.Provider value={{ placementVariable, setPlacementVariable }}>
+    <GlobalContext.Provider value={{ placementVariable, setPlacementVariable, plantData, setPlantData, recipeData, setRecipeData }}>
       {children}
     </GlobalContext.Provider>
+    
   );
 };
