84815e50a0efeab3e1899839f99c74301e87a2cc
SteveHuy <130954328+SteveHuy@users.noreply.github.com>
2023-10-16 18:32:45 +1100

    Add files via upload
    Documentation

--- flask-server/database.py
+++ flask-server/database.py
@@ -5,7 +5,15 @@ from pprint import pprint
 import datetime
 
 class database:
+    """
+    The 'database' class is responsible for connecting to a MongoDB database and handling data retrieval operations.
+    """
     def __init__(self):
+        """
+        Constructor for the 'database' class.
+
+        Initializes the class with a connection to a MongoDB server and relevant collections.
+        """
         uri = "mongodb+srv://user:user@harvest-hero.zdaj74u.mongodb.net/?retryWrites=true&w=majority"
 
         # Create a new client and connect to the server
@@ -28,6 +36,15 @@ class database:
 
     
     def retrieve_area(self, region: str):
+        """
+        Retrieve area data from the database for a specified region and season.
+
+        Parameters:
+        - region (str): The name of the region to retrieve data for.
+
+        Returns:
+        - res (list): A list of documents matching the specified region and season.
+        """
         res = []
         cursor = self.area.find()
         season = self.get_season()
@@ -39,9 +56,22 @@ class database:
     
 
     def retrieve_plant(self, rainfall: int, rainfall_range:int, temperature: int, sun: int, sun_range: int, placement: int):
+        """
+                Retrieve plant data from the database based on specified criteria.
+
+        Parameters:
+        - rainfall (int): The desired rainfall scale.
+        - rainfall_range (int): The desired rainfall range scale.
+        - temperature (int): The desired temperature scale.
+        - sun (int): The desired sunlight scale.
+        - sun_range (int): The desired sunlight range scale.
+        - placement (int): The placement preference (1 for indoors, 2 for outdoors).
+
+        Returns:
+        - res (list): A list of documents matching the specified criteria.
+        """
         cursor = self.crop.find()
         res = []
-
         if placement == 1: #indoors
             for document in cursor:
                 if document['Indoor/Outdoor Preference (1: Indoor,  2: Outdoor)'] == placement:
@@ -56,6 +86,15 @@ class database:
 
 
     def retrieve_recipe(self, plant_id):
+        """
+        Retrieve plant recipes from the database based on the specified plant ID.
+
+        Parameters:
+        - plant_id: The plant ID for which recipes are to be retrieved.
+
+        Returns:
+        - res (list): A list of recipes associated with the specified plant ID.
+        """
         res = []
         cursor = self.recipe.find()
         for document in cursor:
@@ -69,6 +108,12 @@ class database:
 
 
     def get_season(self):
+        """
+        Determine the current season based on the current date.
+
+        Returns:
+        - season (str): The name of the current season.
+        """
         seasons = {
             "Autumn": range(3, 6),   # March to May
             "Winter": range(6, 9),   # June to August
--- flask-server/plant.py
+++ flask-server/plant.py
@@ -3,8 +3,18 @@ from pprint import pprint
 import random
 
 class plant:
-
+    """
+    The 'plant' class provides functionality for selecting plants based on user-defined criteria.
+    """
     def __init__(self, database: database):
+        """
+        Constructor for the 'plant' class.
+
+        Parameters:
+        - database (database): An instance of the 'database' class used for data retrieval.
+
+        Initializes the class with default settings and attributes.
+        """
         #default settings
         self.region = "Karratha"
         self.type = "Fruit" 
@@ -49,20 +59,57 @@ class plant:
     }
     # set the attributes via the front end
     def set_type(self, type: str):
+        """
+        Set the plant type based on user input.
+
+        Parameters:
+        - type (str): The selected plant type.
+
+        Returns:
+        None
+        """
         self.type = type
         return 
     
     
     def set_region(self, region: str):
+        """
+        Set the region based on user input.
+
+        Parameters:
+        - region (str): The selected region.
+
+        Returns:
+        None
+        """
         self.region = region
         return
     
     def set_placement(self, placement: int):
+        """
+        Set the placement based on user input.
+
+        Parameters:
+        - placement (int): The selected placement code.
+
+        Returns:
+        None
+        """
         self.placement = placement
         return 
 
-    # stuff that doesn't need the front end 
     def set_region_factors(self):
+        """
+        Set region-specific factors for rainfall, temperature, and sunlight based on the selected region.
+
+        Retrieves data from the database for the specified region and updates class attributes accordingly.
+
+        Parameters:
+        None
+
+        Returns:
+        None
+        """
         data = self.db.retrieve_area(self.region)
         # sets rainfall, temp and sun
         self.rainfall = data[0]['rain']
@@ -74,9 +121,20 @@ class plant:
         return 
 
     def select_plant(self):
+        """
+        Select suitable plants based on user-defined criteria.
+
+        Retrieves a list of plants from the database that match the specified criteria (rainfall, temperature, sunlight, and placement). Filters the results by plant type, updates the 'plants' attribute, and handles the case when no matching plants are found.
+
+        Parameters:
+        None
+
+        Returns:
+        None
+        """
         self.plants = []
         plants = self.db.retrieve_plant(self.rainfall, self.rainfall_range, self.temp, self.sun, self.sun_range, self.placement)
-        print(plants)
+        pprint(plants)
         for i in plants:
             if i['Type of Plant'] == self.type:
                 self.plants.append(i)
@@ -89,6 +147,17 @@ class plant:
         return 
 
     def set_json_data(self):
+        """
+        Create JSON data for plant recommendations.
+
+        Generates a JSON representation of plant data, including attributes like name, description, recipe ID, recommended sunlight, rainfall, and temperature ranges.
+
+        Parameters:
+        None
+
+        Returns:
+        data (list): A list of JSON data for plant recommendations.
+        """
         data = []
         for i in range(len(self.plants)):
             id = {'_id' : i}
@@ -111,12 +180,34 @@ class plant:
 
 
     def set_recipes(self, plant_id):
+        """
+        Retrieve plant recipes from the database.
+
+        Retrieves plant recipes based on the provided plant ID. Randomly shuffles the recipes and returns up to 5 of them.
+
+        Parameters:
+        - plant_id (str): The ID of the plant for which recipes are to be retrieved.
+
+        Returns:
+        recipes (list): A list of up to 5 plant recipes.
+        """
         recipes = self.db.retrieve_recipe(plant_id)
         if len(recipes) > 5:
             random.shuffle(recipes)
         return recipes[:5]
 
     def print(self):
+        """
+        Print user-defined and calculated plant attributes.
+
+        Prints the selected region, plant type, placement, rainfall, temperature, sunlight, rainfall range, and sunlight range.
+
+        Parameters:
+        None
+
+        Returns:
+        None
+        """
         res = {'Region': self.region,
                'Type': self.type,
                'Placement': self.placement,
